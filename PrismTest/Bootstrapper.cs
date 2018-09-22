b0c446d6aa1c36932e29af842d45fea94a580d78
Wouter <woutervmertens@gmail.com>
2018-09-22 21:03:56 +0200

    Add project files.

---
+++ PrismTest/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using PrismTest.Views;
+using System.Windows;
+using Prism.Modularity;
+using DryIoc;
+using Prism.DryIoc;
+
+namespace PrismTest
+{
+    class Bootstrapper : DryIocBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
